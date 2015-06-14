0727e09ad54349ec30e31592a21c0dd16c055df7
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-06-14 11:33:56 +0300

    webpack react

---
+++ app/main.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var HelloMessage = React.createClass({displayName: "HelloMessage",
+  render: function() {
+    return React.createElement("div", null, "Hello ", this.props.name);
+  }
+});
+
+React.render(React.createElement(HelloMessage, {name: "John"}), document.body);
